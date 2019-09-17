c862025b6f1cb3a4ee1ccb6a30173af319c8b6e1
StanekO <o.stanek@seznam.cz>
2019-09-17 18:28:22 +0200

    windows run daemon --gpu
    windows run daemon --gpu

--- daemon_windows_gpu_4k.cmd
+++ daemon_windows_gpu_4k.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
-ECHO  ###################################
-ECHO  #    GPU 4K renderings requires   #
-ECHO  # instaled http://fractorium.com/ #
-ECHO  ###################################
+ECHO  #######################################
+ECHO  ###    GPU 4K renderings requires   ###
+ECHO  ### instaled http://fractorium.com/ ###
+ECHO  #######################################
 @ECHO ON
 cd %~p0
 title Electric Sheep GPU 4K daemon
