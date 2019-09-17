0f55cab5220a1405b146389fd6e1818bc0157d3b
StanekO <o.stanek@seznam.cz>
2019-09-17 18:19:44 +0200

    windows run daemon --gpu
    windows run daemon --gpu

---
+++ daemon_windows_gpu_4k.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+ECHO  ###################################
+ECHO  #    GPU 4K renderings requires   #
+ECHO  # instaled http://fractorium.com/ #
+ECHO  ###################################
+@ECHO ON
+title Electric Sheep GPU 4K daemon
+cd %~p0
+set PATH=%SYSTEMROOT%\system32;%cd%\contrib\bin
+ruby daemon %* --gpu || pause
